aee6e79b9accd1349ef87d31297cba00f0eb8d33
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-05-24 19:24:36 +0200

    Modify the App

---
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -0,0 +1,27 @@
+import logo from "./logo.jpg";
+import "./App.css";
+import { getFooterCopy, getFullYear } from "./utils";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email:</label>
+        <input type="email" name="email" id="email" />
+        <label htmlFor="password">Password:</label>
+        <input type="password" name="password" id="password" />
+        <button type="button">OK</button>
+      </div>
+      <div className="App-footer">
+        <p>
+          Copyright {getFullYear()} - {getFooterCopy()}
+        </p>
+      </div>
+    </div>
+  );
+}
