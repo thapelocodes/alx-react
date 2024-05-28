25a1e7105f98568709329051d2832443615e9950
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-05-28 07:12:53 +0200

    Deployto a GitHub page

---
+++ 0x01-react_intro/task_4/dashboard/src/Notifications.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./Notifications.css";
+import closeIcon from "./close-icon.png";
+import { getLatestNotification } from "./utils";
+
+export default function Notifications() {
+  return (
+    <div className="Notifications">
+      <button
+        id="closeButton"
+        style={{
+          position: "absolute",
+          right: ".4em",
+          top: ".4em",
+          width: "50px",
+          height: "50px",
+          background: "none",
+          border: "none",
+          cursor: "pointer",
+        }}
+        aria-label="Close"
+        onClick={console.log("Close button has been clicked")}
+      >
+        <img src={closeIcon} alt="closeIcon" width="15px" />
+      </button>
+      <p>Here is the list of notifications</p>
+      <ul>
+        <li data="default">New course available</li>
+        <li data="urgent">New resume available</li>
+        <li
+          data="urgent"
+          dangerouslySetInnerHTML={{ __html: getLatestNotification() }}
+        ></li>
+      </ul>
+    </div>
+  );
+}
---
+++ 0x01-react_intro/task_4/dashboard/src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Notifications from "./Notifications";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Notifications />
+    <App />
+  </React.StrictMode>
+);
+
+reportWebVitals();
