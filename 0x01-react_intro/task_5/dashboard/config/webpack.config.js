34d7eac22c165835545286e54310f25561d8ec5d
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-05-29 12:47:48 +0200

    creating a project using Webpack & installing Babel

---
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,56 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "../dist"),
+    filename: "bundle.js",
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
+  devServer: {
+    static: path.resolve(__dirname, "../dist"),
+    compress: true,
+    open: true,
+    hot: true,
+    port: 9000,
+    liveReload: true,
+  },
+  devtool: "inline-source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      name: "index.html",
+      inject: true,
+      template: "./dist/index.html",
+    }),
+  ],
+};
---
+++ 0x01-react_intro/task_5/dashboard/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import Notifications from "./Notifications";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Notifications />
+    <App />
+  </React.StrictMode>
+);
