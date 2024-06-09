47faa008ac5e53ca5915002f57f8ba6ec7a684b0
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-06-09 20:54:52 +0200

    Notifications

--- 0x05-react_state/task_0/dashboard/src/Notifications/Notifications.js
+++ 0x05-react_state/task_0/dashboard/src/Notifications/Notifications.js
@@ -187,8 +187,6 @@ Notifications.propTypes = {
 Notifications.defaultProps = {
   displayDrawer: false,
   listNotifications: [],
-  handleDisplayDrawer: () => {},
-  handleHideDrawer: () => {},
 };
 
 export default Notifications;
