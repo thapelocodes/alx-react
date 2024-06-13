1faf7123e537191e0b00f7eb8cb4e7df10d98d97
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-06-13 13:12:16 +0000

    another attempt at fixing ES lint issues

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,20 +1,20 @@
-import { Seq } from 'immutable';
+import { Seq } from "immutable";
 
 export default function printBestStudents(object) {
+  const obj = Seq(object)
+    .filter((student) => student.score > 70)
+    .toJS();
+
   function capFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
-  console.log(
-    Seq(object)
-      .filter((student) => student.score > 70)
-      .map((student) => {
-        return {
-          ...student,
-          firstName: capFirstLetter(student.firstName),
-          lastName: capFirstLetter(student.lastName),
-        };
-      })
-      .toJS()
-  );
+  Object.keys(obj).map((student) => {
+    obj[student].firstName = capFirstLetter(obj[student].firstName);
+    obj[student].lastName = capFirstLetter(obj[student].lastName);
+    return obj[student];
+  });
+
+  console.log(obj);
 }
+
