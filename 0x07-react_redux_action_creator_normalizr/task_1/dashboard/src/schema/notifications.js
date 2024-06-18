6818bb7922c14bca0b95b92136de1f92d5f7918f
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-06-18 11:08:00 +0200

    fix file organization

--- 0x07-react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
+++ 0x07-react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
@@ -1,5 +1,8 @@
-import * as notifications from '../../../../notifications.json';
+import * as notificationData from "../../../../notifications.json";
 
 export default function getAllNotificationsByUser(userId) {
-  return notifications.filter((notification) => notification.author.id === userId).map((notification) => notification.context);
+  const notifications = notificationData.default || [];
+  return notifications
+    .filter((notification) => notification.author.id === userId)
+    .map((notification) => notification.context);
 }
--- 0x07-react_redux_action_creator_normalizr/task_1/task_0/dashboard/src/schema/notifications.js
+++
@@ -1,8 +0,0 @@
-import * as notificationData from "../../../../notifications.json";
-
-export default function getAllNotificationsByUser(userId) {
-  const notifications = notificationData.default || [];
-  return notifications
-    .filter((notification) => notification.author.id === userId)
-    .map((notification) => notification.context);
-}
