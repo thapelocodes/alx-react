043955c356829589166d5fe18c9bb416a1b41975
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-06-21 06:52:49 +0200

    Write a basic reducer

---
+++ 0x08-react_redux_reducer_selector/task_0/dashboard/src/reducers/uiReducer.js
@@ -0,0 +1,52 @@
+import {
+  DISPLAY_NOTIFICATION_DRAWER,
+  HIDE_NOTIFICATION_DRAWER,
+  LOGIN,
+  LOGIN_FAILURE,
+  LOGIN_SUCCESS,
+  LOGOUT,
+} from "../actions/uiActionTypes";
+
+export const initialState = {
+  isNotificationDrawerVisible: false,
+  isUserLoggedIn: false,
+  user: {},
+};
+
+export default function uiReducer(state = initialState, action) {
+  switch (action.type) {
+    case DISPLAY_NOTIFICATION_DRAWER:
+      return {
+        ...state,
+        isNotificationDrawerVisible: true,
+      };
+
+    case HIDE_NOTIFICATION_DRAWER:
+      return {
+        ...state,
+        isNotificationDrawerVisible: false,
+      };
+
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        isUserLoggedIn: true,
+      };
+
+    case LOGIN_FAILURE:
+      return {
+        ...state,
+        isUserLoggedIn: false,
+      };
+
+    case LOGOUT:
+      return {
+        ...state,
+        isUserLoggedIn: false,
+      };
+
+    default:
+      break;
+  }
+  return state;
+}
