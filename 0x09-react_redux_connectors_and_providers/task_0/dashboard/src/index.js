850f9a10d0cd4721692fbddad998e5ea54738da5
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-07-01 11:20:48 +0200

    fix store in index.js

--- 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
@@ -2,16 +2,16 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App/App";
 import { createStore } from "redux";
-import uiReducer from "./reducers/uiReducer";
 import { Provider } from "react-redux";
+import uiReducer from "./reducers/uiReducer";
 
 const store = createStore(uiReducer);
 const root = createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
+  <Provider store={store}>
+    <React.StrictMode>
       <App />
-    </Provider>
-  </React.StrictMode>
+    </React.StrictMode>
+  </Provider>
 );
